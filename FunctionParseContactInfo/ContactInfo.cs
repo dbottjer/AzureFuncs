2bc4239b8a0b9b794c021ba0d9bd9f80a358b4be
Bottjer, Dennis <dennis.bottjer@thrivefast.com>
2018-08-19 22:38:18 -0500

    Update

--- FunctionParseContactInfo/ContactInfo.cs
+++ FunctionParseContactInfo/ContactInfo.cs
@@ -11,6 +11,8 @@ namespace FunctionParseContactInfo
         const string WEBSITE_PATTERN = @"^www.";
         const string FACEBOOK_PATTERN = @"^www.Facebook.com";
         const string TWITTER_PATTERN = "^@";
+        const string MONEY_PATTERN = @"^\-?\(?\$?\s*\-?\s*\(?(((\d{1,3}((\,\d{3})*|\d*))?(\.\d{1,4})?)|((\d{1,3}((\,\d{3})*|\d*))(\.\d{0,4})?))\)?$";
+        const string DATE_PATTERN = @"(?n:^(?=\d)((?<month>(0?[13578])|1[02]|(0?[469]|11)(?!.31)|0?2(?(.29)(?=.29.((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(16|[2468][048]|[3579][26])00))|(?!.3[01])))(?<sep>[-./])(?<day>0?[1-9]|[12]\d|3[01])\k<sep>(?<year>(1[6-9]|[2-9]\d)\d{2})(?(?=\x20\d)\x20|$))?(?<time>((0?[1-9]|1[012])(:[0-5]\d){0,2}(?i:\x20[AP]M))|([01]\d|2[0-3])(:[0-5]\d){1,2})?$)";
         #endregion
 
         #region "Reference Data"
@@ -37,6 +39,8 @@ namespace FunctionParseContactInfo
             , new Parser("Phone", PHONE_PATTERN,new List<string>{"phone","office","tel","t","o","p"}, null, new List<string>{"phone","office","tel","t","o","p",":"})
             , new Parser("Fax", PHONE_PATTERN, new List<string>{"fax","f"}, null, new List<string>{"fax","f",":"})
             , new Parser("Cell", PHONE_PATTERN, new List<string>{"cell","mob","m","c"}, null, new List<string>{"cell","mob","m","c",":"})
+            , new Parser("Total", MONEY_PATTERN, new List<string>{"Total"})
+            , new Parser("Date", DATE_PATTERN)
         };
 
         #endregion
--- FunctionParseContactInfo/FunctionParseContactInfo.cs
+++ FunctionParseContactInfo/FunctionParseContactInfo.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -17,21 +20,30 @@ namespace FunctionParseContactInfo
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
-            OCRData ocr = JsonConvert.DeserializeObject<OCRData>(data.ToString());
-
             var ci = new ContactInfo();
 
-            Parallel.ForEach(ocr.Regions, (r) =>
+            try
             {
-                ci.Parse(r);
-            });
+                // Get request body.  Converted to ReadAsStringAync to avoid unicode conversion error.
+                string data = await req.Content.ReadAsStringAsync();
 
+                //dynamic data = await req.Content.ReadAsAsync<object>();
+                OCRData ocr = JsonConvert.DeserializeObject<OCRData>(data.ToString());
+                
+                Parallel.ForEach(ocr.Regions, (r) =>
+                {
+                    ci.Parse(r);
+                });
+            }
+            catch (Exception ex)
+            {
+                log.Info(ex.ToString());
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Unable to parse JSON");
+            }
             var results = new Results(ci.Parsers.Select(p => new { p.Name, p.Value }).ToDictionary(d => d.Name, d => d.Value), ci.UnKnown);
 
             return req.Content == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Unable able to parse JSON")
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Unable to parse JSON")
                 : req.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(results));
 
         }
